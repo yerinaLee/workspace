8764ff648d0bd61e696bb81706a052d5710b8111
lani <poikl11234@naver.com>
2023-09-19 17:42:17 +0900

    JS

--- 04_WebFront/3_JavaScript/js/00_박스.js
+++ 04_WebFront/3_JavaScript/js/00_박스.js
@@ -3,27 +3,52 @@ const box = document.querySelector("#box");
 
 // 적용하기 버튼 클릭 시
 btn.addEventListener("click", function(){
-  // input에 입력된 내용들을 다 불러오고
-  // 순서에 맞게 box에 세팅한다~
   
   const infoAll = document.querySelectorAll(".info");
-  // console.log(infoAll[2].value); // 성공!!
 
-  width = document.querySelector("#width");
-  // console.log(width.value); // ok
-  box.style.width = width.value + "px";
-
+  const width = document.querySelector("#width");
   // box.style.width = infoAll[0].value+ "px";
-  box.style.height = infoAll[1].value+ "px";
-  box.style.fontSize = infoAll[2].value+ "px";
-  box.style.color = infoAll[3].value;
-  box.style.backgroundColor = infoAll[4].value;
-  box.innerHTML = infoAll[5].value;
-  box.style.fontWeight = document.querySelector("[name='f-weight']:checked").value;
-  box.style.textAlign = document.querySelector("[name='h-align']:checked").value;
-  // box.style.verticalAlign = document.querySelector("[name='v-align']:checked").value;
-  // console.log(document.querySelector("[name='v-align']:checked").value);
-})
 
-// 라디오 체크된거 갖고오는법
-// document.querySelector("[name='fw']:checked").value
+  // 너비를 입력하는 input에 값이 작성되어있다면
+  if(width.value.trim().length != 0){
+    box.style.width = width.value + "px";
+  }
+
+  if(infoAll[1].value.trim().length != 0){
+    box.style.height = infoAll[1].value+ "px";
+  }
+
+  if(infoAll[2].value.trim().length != 0){
+    box.style.fontSize = infoAll[2].value + "px";
+  }
+
+  if(infoAll[3].value.trim().length != 0){
+    box.style.color = infoAll[3].value;
+  }
+
+  if(infoAll[4].value.trim().length != 0){
+    box.style.backgroundColor = infoAll[4].value;
+  }
+
+  if(infoAll[5].value.trim().length != 0){
+    box.innerHTML = infoAll[5].value;
+  }
+
+  // f-weight -> 체크된게 없으면 null이 나와요
+              // -> 체크된게 있으면 null 아님
+  if(document.querySelector("[name='f-weight']:checked").value != null){ // 글자 굵기가 체크되었을때
+    // 글자 굵기 적용
+    box.style.fontWeight = document.querySelector("[name='f-weight']:checked").value;
+  }
+
+  
+  if(document.querySelector("[name='h-align']:checked") != null){ // 가로정렬 체크되었을때
+    box.style.justifyContent = document.querySelector("[name='h-align']:checked").value;
+  }
+
+  if(document.querySelector("[name='v-align']:checked") != null){ // 세로정렬 체크되었을때
+    box.style.alignItems = document.querySelector("[name='v-align']:checked").value;
+  }
+
+
+})
