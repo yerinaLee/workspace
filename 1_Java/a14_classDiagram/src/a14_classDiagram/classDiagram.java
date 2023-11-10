fe1cc845c9a586a58a2ff962622fcdbcbecf5ae0
lani <poikl11234@naver.com>
2023-11-10 16:22:43 +0900

    no message

--- 1_Java/a14_classDiagram/src/a14_classDiagram/classDiagram.java
+++ 1_Java/a14_classDiagram/src/a14_classDiagram/classDiagram.java
@@ -2,7 +2,60 @@ package a14_classDiagram;
 
 public class classDiagram {
 	
+	public class BuyCar{
+		private Driver bestDriver;
+		private Car carType;
+		
+		public BuyCar(Driver d) {
+			this.bestDriver = d;
+			this.carType = new Benz();
+		}
+	}
 	
+	
+	public abstract class Car{
+		public void defaultFunction() {}
+		protected abstract void specialFunction();
+	}
+
+	
+	public class Status{
+		private int speed;
+		private String engineStatus;
+		private String oilStatus;
+	}
+	
+	
+	public class Driver{
+		private String name;
+		private int age;
+	}
+	
+	
+	public class Benz extends Car{
+		private static int price;
+		
+		public Status autoDrive(){
+			Status status = new Status();
+			return status;
+		}
+		
+		@Override
+		protected void specialFunction() {}
+	}
+	
+	
+	public class Audi extends Car{
+		private int price;
+		
+		public void saveMode() {}
+		
+		@Override
+		protected void specialFunction() {}
+	}
+	
+	
+
 }
 
 
