9b321e06e1047eb63c6469bd8e0f264649c44b7e
Lani Kim <poikl11234@naver.com>
2024-03-28 20:23:41 +0900

    codingTest

--- y02_codingTest/src/y02_codingTest/Main.java
+++ y02_codingTest/src/y02_codingTest/Main.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
@@ -11,28 +12,19 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-//		
-//		StringTokenizer st;
-//
-//		int[] arr = new int[9];
-//		int num = 0;
-//		int max = 0;
-//		
-//		for(int i=0; i<9; i++) {
-//			arr[i] = Integer.parseInt(br.readLine());
-//			
-//			if(max < arr[i]) {
-//				max = arr[i];
-//				num = i+1;
-//			}
-//		}
-//		
-//		bw.write(max+"\n");
-//		bw.write(num+"\n");
-//		
-//		br.close();
-//		bw.close();
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		String str = "Hello";
+		
+		String str2 = str;
+		
+		bw.write("대입된 str2 : " + str2);
+
+		str = "Hello wooooooorld!!!!";
+		
+		bw.write("\n바뀐 str : " + str);
+		bw.write("\nstr바뀐 후 str2 : " + str2);
+		
+		bw.close();
 	}
 }
