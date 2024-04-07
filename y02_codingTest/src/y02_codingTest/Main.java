0fe01861ddc99ef68199593345f5fef3f6cbe8ba
Lani Kim <poikl11234@naver.com>
2024-04-07 15:05:16 +0900

    codingTest

--- y02_codingTest/src/y02_codingTest/Main.java
+++ y02_codingTest/src/y02_codingTest/Main.java
@@ -22,25 +22,6 @@ public class Main {
 		int[] arr = new int[num];
 		for(int i=0; i<arr.length; i++) arr[i] = i+1;
 		
-		int tmp=0;
-		
-		for(int i=0; i<times; i++) {
-			
-			StringTokenizer st2 = new StringTokenizer(br.readLine());
-			int s = Integer.parseInt(st2.nextToken());
-			int e = Integer.parseInt(st2.nextToken());
-			
-			for(int j=s-1; j<=e-1; j++) {
-				
-				tmp = arr[j];
-				arr[j] = arr[e-1];
-				arr[e-1] = tmp;
-				
-				e--;
-			}
-		}
-		
-		for(int answer : arr) bw.write(answer+" ");
 		
 		br.close();
 		bw.close();
