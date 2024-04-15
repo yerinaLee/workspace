478e1d8017d02e3a40db685c6b2ce7d2a2ecdc7a
Lani Kim <poikl11234@naver.com>
2024-04-15 10:10:04 +0900

    codingTest

--- y02_codingTest/src/y02_codingTest/Main.java
+++ y02_codingTest/src/y02_codingTest/Main.java
@@ -5,6 +5,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
 
@@ -13,9 +14,55 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		String s =  br.readLine();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int i = Integer.parseInt(st.nextToken());
+		int j = Integer.parseInt(st.nextToken());
 		
-		bw.write((int)s.charAt(0)+"");
+		int[][] arr = new int[i][j];
+		
+		for(int n=0; n<i; n++) {
+			
+			String str = br.readLine();
+			
+			for(int m=0; m<j; m++) {
+				if(str.charAt(m) == 'X') {
+					arr[n][m]++;
+				}
+			}
+		}
+		
+		int answer = 0;
+		boolean flag = false;
+		
+		// 행의 빈칸을 파악함
+		for(int n=0; n<i; n++) {
+			
+			flag = false;
+			
+			for(int m=0; m<j; m++) {
+				if(arr[n][m] == 1) {
+					flag = true;
+				}
+			}
+			
+			if(!flag) answer++; 
+		}
+		
+		// 열의 빈칸을 파악함
+		for(int n=0; n<i; n++) {
+			flag = false;
+			for(int m=0; m<j; m++) {
+				if(arr[n][m] == 1) {
+					flag = true;
+				}
+			}
+			
+			if(!flag) answer++; 
+		}
+		
+		
+		
+		bw.write(answer+"");
 		
 		br.close();
 		bw.close();
