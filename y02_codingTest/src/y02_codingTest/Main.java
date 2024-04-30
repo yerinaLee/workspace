af8f2700566d3e74ab0b26f19e75a378c1d75142
Lani Kim <poikl11234@naver.com>
2024-04-30 22:31:19 +0900

    codingTest

--- y02_codingTest/src/y02_codingTest/Main.java
+++ y02_codingTest/src/y02_codingTest/Main.java
@@ -9,7 +9,31 @@ public class Main {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i<t; i++) {
+			
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			int max = Math.max(a, b);
+			int min = Math.min(a, b);
+			
+			int idx = 1;
+			while(true) {
+				
+				if(max * idx % min == 0) {
+					bw.write(max * idx+"\n");
+					break;
+				}
+				
+				idx++;
+			}
+			
+		}
 		
 		br.close();
 		bw.close();
