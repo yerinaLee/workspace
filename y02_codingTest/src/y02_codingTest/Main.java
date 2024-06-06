33a18db54de88ba749fb62930c983f90d31aabc1
Lani Kim <poikl11234@naver.com>
2024-06-06 21:19:03 +0900

    codingTest

--- y02_codingTest/src/y02_codingTest/Main.java
+++ y02_codingTest/src/y02_codingTest/Main.java
@@ -10,33 +10,14 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		// 세로읽기
 		
 		
-		String[] arr = new String[5];
-		
-		for(int i=0; i<5; i++) {
-			arr[i] = br.readLine();
-		}
-		
-		String answer = "";
-		
-		for(int col = 0; col<5; col++) {
-			for(int row = 0; row<5; row++) {
-				
-				if((arr[row].charAt(col))+"" == "") continue;
-				
-				answer += arr[row].charAt(col);
-			}
-		}
-		
-		bw.write(answer);
-				
-				
 		br.close();
 		bw.close();
 	}
+	
 }
+
 
 
 
