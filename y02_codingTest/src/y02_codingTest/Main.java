bdeed9f3a51d4d48631259f2c6ecccc435d69b2c
Lani Kim <poikl11234@naver.com>
2024-06-22 23:40:43 +0900

    codingTest

--- y02_codingTest/src/y02_codingTest/Main.java
+++ y02_codingTest/src/y02_codingTest/Main.java
@@ -9,19 +9,29 @@ public class Main {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int t = Integer.parseInt(br.readLine());
+		List<Map<Object, Object>> list = new ArrayList<Map<Object,Object>>();
 		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		long n = Integer.parseInt(st.nextToken());
-		long m = Integer.parseInt(st.nextToken());
-		
-		long sum = n-m;
-		
-		if(sum < 0) sum *= -1;
-		
-		bw.write(sum+"");
+		for(int i=0; i<t; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			
-			
+			Map<Object, Object> map = new HashMap<Object, Object>();
+			map.put("age", Integer.parseInt(st.nextToken()));
+			map.put("name", st.nextToken());
+
+			list.add(map);
+		}
+		
+		for(int i=0; i<t; i++) {
+			for(int j=i+1; j<t; j++) {
+				
+				
+			}
+		}
+		
+		
+		
 		bw.flush();
 		br.close();
 		bw.close();
