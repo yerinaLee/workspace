e80446923bdeedf9409d8a40974f0df0496d6de0
Lani Kim <poikl11234@naver.com>
2024-06-30 21:12:54 +0900

    codingTest

--- y02_codingTest/src/y02_codingTest/Main.java
+++ y02_codingTest/src/y02_codingTest/Main.java
@@ -10,21 +10,6 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		
-		List<Long> list = new ArrayList<Long>();
-		
-		StringTokenizer st2 = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++) {
-			list.add(Long.parseLong(st2.nextToken()));
-		}
-		
-		Collections.sort(list);
-		
-		bw.write(list.get(K-1)+"");
-		
 		bw.flush();
 		br.close();
 		bw.close();
