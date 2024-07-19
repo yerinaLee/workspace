4c42a551a03bcf5da1a33883e0b5e8e2c4300f70
Lani Kim <poikl11234@naver.com>
2024-07-19 23:21:55 +0900

    codingTest

--- y02_codingTest/src/y02_codingTest/Main.java
+++ y02_codingTest/src/y02_codingTest/Main.java
@@ -4,18 +4,43 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
+	
 	public static void main(String[] args) throws IOException {
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		int[][] arr = new int[N][M];
+		for(int i=0; i<N; i++) {
+			
+			String str = br.readLine();
+
+			for(int j=0; j<M; j++) {
+				if(str.charAt(j) == 'W') arr[i][j] = 1; 
+				else  arr[i][j] = 0; 
+			}
+		}
+		
+		for(int i=0; i<N-i; i++) {
+			
+			for(int j=0; j<; j++) {
+				
+			}
+		}
+		
+		
+		
+		
+		
 		
 		bw.flush();
 		br.close();
 		bw.close();
 	}
 }
-
-
 
 
