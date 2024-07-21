41bc86511a791cab729da31cd4629a72d985a09e
Lani Kim <poikl11234@naver.com>
2024-07-21 21:23:28 +0900

    codingTest

--- y02_codingTest/src/y02_codingTest/Main.java
+++ y02_codingTest/src/y02_codingTest/Main.java
@@ -25,17 +25,38 @@ public class Main {
 			}
 		}
 		
-		for(int i=0; i<N-i; i++) {
-			
-			for(int j=0; j<; j++) {
+		List<Integer> list = new ArrayList<Integer>();
+		Boolean flag = false;
+		
+		for(int i=0; i<=N-8; i++) {
+			for(int j=0; j<=M-8; j++) {
 				
+				int sum = 0;
+				
+				for(int x=0; x<8; x++) {
+					for(int y=0; y<7; y++) {
+						if(arr[x][y] == arr[x][y+1]) {
+							sum++;
+							y++;
+						}
+					}
+				}
+				
+				if(sum == 0) {
+					flag = true;
+					break;
+				}
+				else list.add(sum);
 			}
+			
+			if(flag) break;
 		}
 		
-		
-		
-		
-		
+		if(flag) bw.write(0+"");
+		else {
+			Collections.sort(list);
+			bw.write(list.get(0));
+		}
 		
 		bw.flush();
 		br.close();
