04fd22ca6ab37136a793d049f44c741854587042
Lani Kim <poikl11234@naver.com>
2024-07-28 21:12:48 +0900

    codingTest

--- y02_codingTest/src/y02_codingTest/Main.java
+++ y02_codingTest/src/y02_codingTest/Main.java
@@ -10,18 +10,6 @@ public class Main {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int M = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-		
-		for(int i=0; i<M; i++) {
-			
-			int card = Integer.parseInt(st.nextToken());
-			
-			if(map.containsKey(card)) map.put(card, map.get(card)+1);
-			else map.put(card , 1);
-		}
-		
 		bw.flush();
 		br.close();
 		bw.close();
